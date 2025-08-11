3f111f626645742471266d67bff765a2946517c2
rempek99 <arkadiusz.remplewicz@gmail.com>
2025-08-11 17:34:04 +0200

    Public path fixed

--- src/main/java/com/zimnyciechan/eventice/auth/config/SecurityConfig.java
+++ src/main/java/com/zimnyciechan/eventice/auth/config/SecurityConfig.java
@@ -9,7 +9,6 @@ package com.zimnyciechan.eventice.auth.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,11 +17,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
@@ -39,25 +35,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/api/public/**").permitAll()
-                .anyRequest().authenticated());
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(withDefaults());
         http.sessionManagement(
                 session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.csrf(AbstractHttpConfigurer::disable);
-//        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
         return http.build();
     }
-
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user1 = User.withUsername("user1").password("{noop}password1").roles("USER").build();
-//        UserDetails admin = User.withUsername("admin").password("{noop}adminPass").roles("ADMIN").build();
-//
-//        return new InMemoryUserDetailsManager(user1, admin);
-//    }
-
 
     @Bean
     public UserDetailsService userDetailsService() {
