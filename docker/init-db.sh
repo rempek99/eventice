caecb304837db6b66ed5ac6807c094def0d0378a
Arkadiusz Remplewicz <58391537+rempek99@users.noreply.github.com>
2025-11-25 22:47:58 +0100

    45 contenerize backend app (#48)
    * Backend project refactored, deploy Dockerfile implemented, local run script added
    
    * Dockerfile build and deploy implemented
    
    * Contenerize db and backend fixed
    
    * .env file removed from repo
    
    * Removed secrets
    
    * git action working directory fixed

--- docker/init-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Tworzenie użytkownika i bazy danych
-psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
-    CREATE USER backend WITH PASSWORD 'VdjzymyUHSwBsDFAs2yel0fPsxVLWrA6';
-    CREATE DATABASE backend_database OWNER backend;
-EOSQL
--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
--- insert into USERS(ID,EMAIL,PASSWORD,USERNAME) values (-1,'testing@test.com','2c73bdccfcb396e58ede6691fb9ca773','test1')
--- insert into USERS(ID,EMAIL,PASSWORD,USERNAME) values (-2,'testing2@test.com','2c73bdccfcb396e58ede6691fb9ca773','test2')
--- insert into USERS(ID,EMAIL,PASSWORD,USERNAME) values (-3,'testing3@test.com','2c73bdccfcb396e58ede6691fb9ca773','test3')
--- insert into USERS(ID,EMAIL,PASSWORD,USERNAME) values (-4,'testing4@test.com','2c73bdccfcb396e58ede6691fb9ca773','test4')
-select * from USERS;
